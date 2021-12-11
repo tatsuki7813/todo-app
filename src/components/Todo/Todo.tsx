8a15d4d7a225e7cb7bb7716899808bbfe4a97275
kobayashi.tatsuki <ttk7813@gmail.com>
2021-12-11 21:32:45 +0900

    TODOを入力するための親コンポーネントであるTodoInputFormコンポーネントを作成

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -2,12 +2,14 @@ import { VFC } from "react";
 import { TitleLabel } from "@/libs";
 import { CompleteTodos } from "./CompleteTodos";
 import { IncompleteTodos } from "./IncompleteTodos";
+import { TodoInputForm } from "./TodoInputForm";
 
 export const Todo: VFC = () => {
   return (
     <>
       <TitleLabel text="TODO管理" tag="h1" />
       <TitleLabel text="TODO入力" />
+      <TodoInputForm />
       <TitleLabel text="未完了のTODO" />
       <IncompleteTodos />
       <TitleLabel text="完了のTODO" />
---
+++ src/components/Todo/TodoInputForm/TodoInputForm.tsx
@@ -0,0 +1,5 @@
+import { VFC } from "react";
+
+export const TodoInputForm: VFC = () => {
+  return <p>todo input form components.</p>;
+};
